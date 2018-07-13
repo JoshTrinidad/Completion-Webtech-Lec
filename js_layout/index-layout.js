52a064e4054133a7c670db00cb4042437bbd81f1
ParisLovelyn <lovelynparis26@gmail.com>
2018-07-13 16:46:28 +0800

    Unzipped current project

---
+++ js_layout/index-layout.js
@@ -0,0 +1,7 @@
+function on() {
+    document.getElementById("ssws-overlay-container").style.display = "block";
+}
+
+function off() {
+    document.getElementById("ssws-overlay-container").style.display = "none";
+}
